10faacbcca8dc2cfcff1660bbe0d993a1b40d1f6
Thrcle421 <Thrcle421@gmail.com>
2025-01-01 23:45:07 -0800

    Optimized the page layout and added pagination functionality

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, Http404
 from django.shortcuts import render, reverse, redirect
 from django.urls.base import reverse_lazy
 from django.views.decorators.http import require_http_methods, require_POST, require_GET
+from django.core.paginator import Paginator
 
 from blog.forms import PubBlogForm
 from blog.models import BlogCategory, Blog, BlogComment
@@ -12,22 +13,35 @@ from django.db.models import Q
 
 
 def index(request):
-    blogs = Blog.objects.all()
-    return render(request, "index.html", context={"blogs": blogs})
+    page = request.GET.get('page', 1)
+    blogs = Blog.objects.all().order_by('-pub_time')
+    paginator = Paginator(blogs, 2)  # 10 posts per page
+    page_obj = paginator.get_page(page)
+    return render(request, "index.html", context={"page_obj": page_obj})
+
 
 def blog_detail(request, blog_id):
     try:
         blog = Blog.objects.get(pk=blog_id)
+        # Get comments and paginate
+        page = request.GET.get('page', 1)
+        comments = blog.comments.all().order_by('-pub_time')
+        paginator = Paginator(comments, 2)  # 10 comments per page
+        comments_page = paginator.get_page(page)
+        return render(request, "blog_detail.html", {
+            "blog": blog,
+            "comments_page": comments_page
+        })
     except Blog.DoesNotExist:
         raise Http404
-    return render(request, "blog_detail.html", {"blog": blog})
+
 
 @require_http_methods(["GET", "POST"])
 @login_required()
 def pub_blog(request):
     if request.method == "GET":
-        categories=BlogCategory.objects.all()
-        return render(request, "pub_blog.html",context={"categories": categories})
+        categories = BlogCategory.objects.all()
+        return render(request, "pub_blog.html", context={"categories": categories})
     else:
         forms = PubBlogForm(request.POST)
         print(forms)
@@ -35,22 +49,39 @@ def pub_blog(request):
             title = forms.cleaned_data.get("title")
             category_id = forms.cleaned_data.get("category")
             content = forms.cleaned_data.get("content")
-            blog = Blog.objects.create(title=title, content=content, category_id=category_id, author=request.user)
+            blog = Blog.objects.create(
+                title=title, content=content, category_id=category_id, author=request.user)
             return JsonResponse({'code': 200, 'message': 'blog is successfully released', 'data': {'blog_id': blog.id}})
         else:
             print(forms.errors)
             return JsonResponse({'code': 400, 'message': 'bad request'})
+
 
 @require_POST
 @login_required()
 def pub_comment(request):
     blog_id = request.POST.get("blog_id")
     content = request.POST.get("content")
-    BlogComment.objects.create(blog_id=blog_id, content=content,author=request.user)
-    return redirect(reverse('blog:blog_detail',kwargs={'blog_id': blog_id}))
+    BlogComment.objects.create(
+        blog_id=blog_id, content=content, author=request.user)
+    return redirect(reverse('blog:blog_detail', kwargs={'blog_id': blog_id}))
+
 
 @require_GET
 def search_blog(request):
-    q=request.GET.get("q")
-    blogs = Blog.objects.filter(Q(title__icontains=q) | Q(content__icontains=q)).all()
-    return render(request, 'index.html', {'blogs': blogs})
+    query = request.GET.get('q', '')
+    if query:
+        blogs = Blog.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+        ).order_by('-pub_time')
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(blogs, 2)
+        page_obj = paginator.get_page(page)
+
+        return render(request, "index.html", {
+            "page_obj": page_obj,
+            "search_query": query
+        })
+    return redirect('blog:index')
